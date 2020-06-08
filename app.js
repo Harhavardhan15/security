1a4e0ccd497324702913d7d47afa1f8a44a59adc
M <578150@cognizant.com>
2020-06-08 16:33:07 +0530

    Adding Bcrypt to project with hash

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -23,8 +24,8 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret:secret ,encryptedFields: ["password"]  });
+const secret = "Thi"
+
 const User = new mongoose.model("User", userSchema);
 
 
@@ -41,31 +42,43 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-     const newUser = new User({
-         email:req.body.username,
-         password:req.body.password
-     });
-     newUser.save(function(err){
-         if(!err){
-             res.render("secrets");
-         }else{
-             console.log(err);
-         }
-     });
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email:req.body.username,
+            
+            password:hash
+        });
+        newUser.save(function(err){
+            if(!err){
+                res.render("secrets");
+            }else{
+                console.log(err);
+            }
+        });
+    });
+    
 });
 
 app.post("/login",function(req,res){
+    
    const username = req.body.username;
    const password = req.body.password;
    User.findOne({email: username},function(err,foundUser){
-    if(foundUser.password === password){
-        res.render("secrets");
-    }else{
-        console.log(err);
-    }
-   })
+       if(err){
+           console.log(err);
+       }else{
+    bcrypt.compare(password, foundUser.password, function(err, result) {
+        // result == true
+        if(result === true){ 
+            res.render("secrets");
+        }else{
+            console.log(err);
+        }
+    })
+}
 })
+   });
 
-app.listen(3000,function(){
-    console.log("Server Running Successfully on PORT 3000");
-});
+   app.listen(3000,function(){
+       console.log("server running 3000...")
+   })
